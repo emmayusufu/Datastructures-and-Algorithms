33df6825c1341ac62924fa5dc6b43fbc70e92c5d
Emmanuel Yusufu Kimaswa <kimaswaemma36@gmail.com>
2025-01-20 21:55:59 +0300

    added hash table

--- data-structures/javascript/hashing_and_hash_tables.js
+++ data-structures/javascript/hashing_and_hash_tables.js
@@ -1,11 +1,56 @@
-/**
-Hashing
-Hashing is a process where data is mapped to a fixed-size value (called a hash) using a hash function.
-The hash function takes an input (e.g., a string or a number) and produces a deterministic and fixed-size output.
+class CustomHashTable {
+  constructor(size = 5) {
+    this.table = new Array(size);
+  }
 
-Characteristics of a Good Hash Function
-    - Deterministic: The same input always produces the same output.
-    - Uniform Distribution: Outputs are evenly distributed, minimizing collisions.
-    - Fast Computation: The hash function should be efficient to compute.
-    - Collision Resistance: It minimizes the likelihood of two different inputs producing the same hash (though it can't eliminate it).
-*/
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+    const result = hash % this.table.length;
+    return result;
+  }
+
+  set(key, value, ttl = 60) {
+    const index = this._hash(key);
+    if (!this.table[index]) {
+      this.table[index] = [];
+    }
+    const expiration = Date.now() + ttl * 1000;
+    this.table[index].push([key, value, expiration]);
+  }
+
+  delete(key) {
+    const index = this._hash(key);
+    if (!index) return 0;
+
+    this.table.splice(index, 1);
+  }
+
+  get(key) {
+    const index = this._hash(key);
+    if (this.table[index]) {
+      for (let [storedKey, value, expiration] of this.table[index]) {
+        if (storedKey === key) {
+          if (Date.now() > expiration) {
+            this.delete(key);
+            return undefined;
+          }
+          return value;
+        }
+      }
+    }
+    return undefined;
+  }
+}
+
+const hashTable = new CustomHashTable();
+hashTable.set("name", "Alice");
+hashTable.set("age", 30);
+hashTable.set("city", "New York");
+hashTable.set("username", "alice123");
+console.log(hashTable.table);
+console.log(hashTable.get("age"));
+console.log(hashTable.delete("age"));
+console.log(hashTable.table);
